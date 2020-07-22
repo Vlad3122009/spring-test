56afbfa168ce614bd8c80b8d4c3aee239891cf26
Stranger09 <stranger1995.ay@gmail.com>
2020-07-22 20:06:19 +0300

    First commit

---
+++ src/main/java/com/testingspring/springtest/AppContext.java
@@ -0,0 +1,7 @@
+package com.testingspring.springtest;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppContext {
+}
---
+++ src/main/java/com/testingspring/springtest/SiteController.java
@@ -0,0 +1,7 @@
+package com.testingspring.springtest;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SiteController {
+}
